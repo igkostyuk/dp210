a6222be1ca09e7755e4672c519bff035a37bd88e
Igor kostyuk <ig.fire@gmail.com>
2021-07-21 23:12:26 +0300

    refactor: task3

--- triangle/main.go
+++ triangle/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
+	"io"
 	"math"
 	"os"
 	"sort"
@@ -12,132 +13,127 @@ import (
 )
 
 var (
-	confirms             = []string{"y", "yes"}
-	triangleParamsString = "<name>,<a>,<b>,<c>"
+	confirms = []string{"y", "yes"}
 
-	ErrSizeSyntax       = errors.New("size should be positive float")
+	// ErrSizeSyntax indicates that a value does not have the right syntax for the size type.
+	ErrSizeSyntax = errors.New("size should be positive float")
+	// ErrParametersLength indicates that program called with wrong number of parameters
 	ErrParametersLength = errors.New("expected 4 comma separated parameters")
-	ErrParametersValid  = errors.New("not valid sizes: expected a + b <= c or a + c <= b or b + c <= a")
+	// ErrSizeRatio indicates that triangle size ratio is wrong.
+	ErrSizeRatio = errors.New("not valid sizes: expected a + b <= c or a + c <= b or b + c <= a")
 )
 
+// Triangle represent triangle.
 type Triangle struct {
 	name    string
 	a, b, c float64
-	area    float64
 }
 
-func NewTriangle(name string, a, b, c float64) *Triangle {
-	// Heron's formula
-	s := (a + b + c) / 2
-	area := math.Sqrt(s * (s - a) * (s - b) * (s - c))
+// NewTriangle create triangle with name and sizes.
+func NewTriangle(name string, a, b, c float64) (*Triangle, error) {
+	if a <= 0 || b <= 0 || c <= 0 {
+		return nil, ErrSizeSyntax
+	}
+	if a+b <= c || a+c <= b || b+c <= a {
+		return nil, ErrSizeRatio
+	}
+	return &Triangle{name: name, a: a, b: b, c: c}, nil
+}
 
-	return &Triangle{name: name, a: a, b: b, c: c, area: area}
+// Area return triangle area.
+func (t *Triangle) Area() float64 {
+	// Heron's formula
+	s := (t.a + t.b + t.c) / 2
+	return math.Sqrt(s * (s - t.a) * (s - t.b) * (s - t.c))
+}
+
+func (t *Triangle) String() string {
+	return fmt.Sprintf("[Triangle %s]: %.2f cm", t.name, t.Area())
+}
+
+type reader struct {
+	br *bufio.Reader
+}
+
+func newReader(r io.Reader) *reader {
+	return &reader{br: bufio.NewReader(r)}
 }
 
 func sanitize(text string) string {
 	text = strings.ReplaceAll(text, " ", "")
 	text = strings.ReplaceAll(text, "\t", "")
-
+	text = strings.ReplaceAll(text, "\n", "")
 	return strings.ToLower(text)
 }
 
-func getTriangle(s *bufio.Scanner, msg string) (*Triangle, error) {
-	fmt.Printf("Enter triangle %s: ", msg)
-	if s.Scan() {
-		params := strings.Split(sanitize(s.Text()), ",")
-		name, a, b, c, err := parseParams(params)
-		if err != nil {
-			return nil, err
-		}
-
-		if a+b <= c || a+c <= b || b+c <= a {
-			return nil, ErrParametersValid
-		}
-
-		return NewTriangle(name, a, b, c), nil
+func (r *reader) ReadTriangle(delim byte) (*Triangle, error) {
+	text, err := r.br.ReadString(delim)
+	if err != nil {
+		return nil, fmt.Errorf("reading triangle:%w", err)
 	}
-
-	return nil, s.Err()
-}
-
-func parseParams(params []string) (name string, a, b, c float64, err error) {
+	params := strings.Split(sanitize(text), ",")
 	if len(params) != 4 {
-		err = ErrParametersLength
-
-		return
+		return nil, ErrParametersLength
 	}
-	name = params[0]
-	a, err = strconv.ParseFloat(params[1], 64)
-	if err != nil || a <= 0 {
-		err = fmt.Errorf("a :%w", ErrSizeSyntax)
-
-		return
-	}
-
-	b, err = strconv.ParseFloat(params[2], 64)
-	if err != nil || b <= 0 {
-		err = fmt.Errorf("b :%w", ErrSizeSyntax)
-
-		return
-	}
-
-	c, err = strconv.ParseFloat(params[3], 64)
-	if err != nil || c <= 0 {
-		err = fmt.Errorf("c :%w", ErrSizeSyntax)
-	}
-
-	return
-}
-
-func isDone(s *bufio.Scanner, confirms []string) bool {
-	fmt.Printf("continue %v ?:", confirms)
-	if !s.Scan() {
-		return true
-	}
-	for _, word := range confirms {
-		if strings.EqualFold(s.Text(), word) {
-			return false
+	var s [3]float64
+	for i, p := range params[1:] {
+		if s[i], err = strconv.ParseFloat(p, 64); err != nil {
+			return nil, ErrSizeSyntax
 		}
 	}
+	return NewTriangle(params[0], s[0], s[1], s[2])
 
-	return true
 }
 
-func printTriangles(ts []*Triangle) {
-	fmt.Println("============= Triangles list: ===============")
-	for i, t := range ts {
-		fmt.Printf("%d. [Triangle %s]: %.2f cm\n", i+1, t.name, t.area)
+func confirm(r io.Reader, w io.Writer, confirms []string) bool {
+	fmt.Fprintf(w, "continue %v ?:", confirms)
+	br := bufio.NewReader(r)
+	text, err := br.ReadString('\n')
+	if err == nil {
+		text = strings.TrimSpace(text)
+		for _, word := range confirms {
+			if strings.EqualFold(text, word) {
+				return true
+			}
+		}
 	}
+	return false
 }
 
-func Task(s *bufio.Scanner) error {
-	var ts []*Triangle
-	var done bool
+// Task read tringles from strign and write sorter by area.
+func Task(r io.Reader, w io.Writer) error {
+	var (
+		ts   []*Triangle
+		done bool
+	)
+	tr := newReader(r)
 	for !done {
-		t, err := getTriangle(s, triangleParamsString)
-		if err != nil {
-			fmt.Println(err)
-		}
-		if err == nil && t != nil {
+		fmt.Fprintf(w, "Enter triangle <name>,<a>,<b>,<c>: ")
+		switch t, err := tr.ReadTriangle('\n'); err {
+		case nil:
 			ts = append(ts, t)
+		default:
+			fmt.Fprintln(w, err)
 		}
-		done = isDone(s, confirms)
+		done = !confirm(r, w, confirms)
 	}
-	sort.Slice(ts, func(i, j int) bool { return ts[i].area > ts[j].area })
-	printTriangles(ts)
+	sort.Slice(ts, func(i, j int) bool { return ts[i].Area() > ts[j].Area() })
+	fmt.Fprintln(w, "============= Triangles list: ===============")
+	for i, t := range ts {
+		fmt.Fprintf(w, "%d. %s", i+1, t)
+	}
 
-	return s.Err()
+	return nil
 }
 
-func usage() {
-	fmt.Fprintf(os.Stdout, "%s: sorts the triangles from user input\n", os.Args[0])
+func usage(w io.Writer) {
+	fmt.Fprintf(w, "%s: sorts the triangles from user input\n", os.Args[0])
 }
 
 func main() {
-	usage()
-	scanner := bufio.NewScanner(os.Stdin)
-	if err := Task(scanner); err != nil {
+	usage(os.Stdout)
+	if err := Task(os.Stdin, os.Stdout); err != nil {
 		fmt.Println(err)
-		os.Exit(1)
+		os.Exit(0)
 	}
 }
